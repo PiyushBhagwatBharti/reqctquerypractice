d2bfafa94a27343c99d66c51f03f3abe2cc2e492
PiyushBhagwatBharti <piyush.bhagwat@bhartisofttech>
2025-12-17 12:44:13 +0530

    added formik

---
+++ components/Form.jsx
@@ -0,0 +1,72 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { Field, FormikProvider, useFormik } from "formik";
+import React from "react";
+
+async function createPost(newPost) {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: newPost,
+  });
+  if (!res.ok) {
+    console.log("Error in posting");
+    throw new Error("error in posting");
+  }
+  return await res.json();
+}
+
+const Form = () => {
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation({
+    mutationFn: createPost,
+    // onSuccess: () => {
+    //   queryClient.invalidateQueries(["posts"]);
+    // },
+    onMutate: (newPost) => {
+      queryClient.cancelQueries(["posts"]);
+      const prevPosts = queryClient.getQueryData(["posts"]); //save the previous state for error handeling
+      queryClient.setQueryData(["posts"], (old) => [
+        ...old,
+        { ...newPost, id: Date.now() },
+      ]);
+
+      return { prevPosts }; //return pre state for the same reason
+    },
+
+    onError: (er, newPost, context) => {
+      queryClient.setQueryData(["posts"], context.prevPosts);
+    },
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      body: "",
+    },
+    onSubmit: (val) => {
+        alert(JSON.stringify(val));
+      mutate(val);
+    },
+  });
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <input
+        name="title"
+        placeholder="Title"
+        onChange={formik.handleChange}
+        value={formik.values.title}
+      />
+
+      <textarea
+        name="body"
+        placeholder="blog body"
+        onChange={formik.handleChange}
+        value={formik.values.body}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default Form;
--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,15 @@
 import { useState } from "react";
-import "./App.css";
 import Posts from "../components/Posts";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import Form from "../components/Form";
 
-async function createPost(newPost) {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
-    method: "POST",
-    body: newPost,
-  });
-  if (!res.ok) {
-    console.log("Error in posting");
-    throw new Error("error in posting");
-  }
-  return await res.json();
-}
 
 function App() {
-  const [title, setTitle] = useState("");
   const [toggle, setToggle] = useState(false);
-  const queryClient = useQueryClient();
-
-  const { mutate } = useMutation({
-    mutationFn: createPost,
-    // onSuccess: () => {
-    //   queryClient.invalidateQueries(["posts"]);
-    // },
-    onMutate: (newPost) => {
-      queryClient.cancelQueries(["posts"]);
-      const prevPosts = queryClient.getQueryData(["posts"]); //save the previous state for error handeling
-      queryClient.setQueryData(["posts"], (old) => [
-        ...old,
-        { ...newPost, id: Date.now() },
-      ]);
-
-      return { prevPosts }; //return pre state for the same reason
-    },
-
-    onError: (er, newPost, context) => {
-      queryClient.setQueryData(["posts"], context.prevPosts);
-    },
-  });
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-    mutate({ title, body: "This is body" });
-  }
+  
 
   return (
     <>
-      <input type="text" onChange={(e) => setTitle(e.target.value)} />
-      <button onClick={handleSubmit}>submit</button>
+      <Form />
       <button onClick={() => setToggle((p) => !p)}>toggle</button>
       {toggle && <Posts />}
     </>
