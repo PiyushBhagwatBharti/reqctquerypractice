0f6cf5bbe5ea3c61bc14800288865baa007083d1
PiyushBhagwatBharti <piyush.bhagwat@bhartisofttech>
2025-12-17 10:45:54 +0530

    added mutation function adn optimistic updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,51 @@
 import { useState } from "react";
 import "./App.css";
 import Posts from "../components/Posts";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-async function createPost() {
+async function createPost(newPost) {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
     method: "POST",
+    body: newPost,
   });
+  if (!res.ok) {
+    console.log("Error in posting");
+    throw new Error("error in posting");
+  }
+  return await res.json();
 }
 
 function App() {
   const [title, setTitle] = useState("");
   const [toggle, setToggle] = useState(false);
+  const queryClient = useQueryClient();
 
-  async function handleSubmit(e){
-      e.preventDefault();
+  const { mutate } = useMutation({
+    mutationFn: createPost,
+    // onSuccess: () => {
+    //   queryClient.invalidateQueries(["posts"]);
+    // },
+    onMutate: (newPost) => {
+      queryClient.cancelQueries(["posts"]);
+      queryClient.setQueryData(["posts"], (old) => [
+        ...old,
+        { ...newPost, id: Date.now() },
+      ]);
+      const prevPosts = queryClient.getQueryData(["posts"]);
+
+      return prevPosts;
+    },
+  });
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    mutate({ title, body: "This is body" });
   }
 
   return (
     <>
       <input type="text" onChange={(e) => setTitle(e.target.value)} />
-      <button>submit</button>
+      <button onClick={handleSubmit}>submit</button>
       <button onClick={() => setToggle((p) => !p)}>toggle</button>
       {toggle && <Posts />}
     </>
